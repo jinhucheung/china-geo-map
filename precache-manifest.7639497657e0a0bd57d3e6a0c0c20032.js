65a90caa0512f58cb4b4c11a53d885157496893d
jinhucheung <hi.jinhu.zhang@gmail.com>
2020-03-08 09:39:02 +0000

    Deploying to gh-pages from  - e213b8f7897be27b2b3de34e3570f9e968213413 🚀

---
+++ precache-manifest.7639497657e0a0bd57d3e6a0c0c20032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadd841f9294a5540e637f62d458b2f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb0b601b4322613a1fbd",
+    "url": "./static/css/main.41df844e.chunk.css"
+  },
+  {
+    "revision": "3c82d098a7fef3f396ff",
+    "url": "./static/js/2.d66e52a6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.d66e52a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0b601b4322613a1fbd",
+    "url": "./static/js/main.2c748308.chunk.js"
+  },
+  {
+    "revision": "dbeb5ef0e5dd00c90f5d",
+    "url": "./static/js/runtime-main.d8dd5aaf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jinhucheung/china-geo-map/precache-manifest.dc97620bc88a8483038807bcd44ee160.js"
+  "./precache-manifest.7639497657e0a0bd57d3e6a0c0c20032.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jinhucheung/china-geo-map/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
