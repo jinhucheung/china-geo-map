155f322fae58cddab6f14d374e49f5606368b90c
jinhucheung <hi.jinhu.zhang@gmail.com>
2020-03-08 13:30:58 +0000

    Deploying to gh-pages from  - 629c9b0a5a9776e8b35f75dea91cc48f785c9e83 🚀

---
+++ precache-manifest.a8c9ca79660bd24917f30c10002ac80a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdce203cfccfc5599f65a0ec033d1b73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6ed3fb834a1788a0961",
+    "url": "./static/css/main.c4a03584.chunk.css"
+  },
+  {
+    "revision": "e3ff77fbbf5c35b52f5b",
+    "url": "./static/js/2.ce3fe247.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.ce3fe247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ed3fb834a1788a0961",
+    "url": "./static/js/main.f434c2e6.chunk.js"
+  },
+  {
+    "revision": "dbeb5ef0e5dd00c90f5d",
+    "url": "./static/js/runtime-main.d8dd5aaf.js"
+  }
+]);
